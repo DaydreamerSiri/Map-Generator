e7794deb9008526cb9b40c6e1c17d0b1b461ccfa
SugMadicx <147711385+SugMadicx@users.noreply.github.com>
2023-11-20 13:35:49 +0100

    Updated Geography File (for me)

--- MapGenerator/src/controller/Geography.java
+++ MapGenerator/src/controller/Geography.java
@@ -1,30 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
-import java.util.ArrayList;
-import java.util.List;
 
-/**
- *
- * @author jason
- */
+//@author jason
+
 public class Geography {     
-    public Object[] geographyGen(){
+    public Object[] geographyGen(int runningBonus){
         DiceRoll diceRoll = new DiceRoll();
-        int runningBonus = 0;
-        boolean mapFull = false;
-        
-        //System.out.println("Number of Tiles: " + numTiles);
-        //System.out.println("Map Full? " + mapFull);
         
         int roll = diceRoll.roll(100);
         int num = roll + runningBonus;
             
-        System.out.println("Rolled: " + roll);
-        System.out.println("Bonus: " + runningBonus);
-        System.out.println("Number: " + num);
+        //System.out.println("Rolled: " + roll);
+        //System.out.println("Bonus: " + runningBonus);
+        //System.out.println("Number: " + num);
             
         //check and balance of running bonus
         if(num >= 101){
@@ -34,88 +21,86 @@ public class Geography {
         }
         
         
-        return tableGeography(num);
-            /*numTiles--;
-            if(numTiles == 0){
-                mapFull = true;
-                System.out.println("Map is full!");
-            }*/
+        return tableGeography(num, runningBonus);
     }
     
-    public Object[] tableGeography(int num){
-        Object[] typeAndNum = new Object[2];
+    public Object[] tableGeography(int num, int runningBonus){
+        Object[] tileInfo = new Object[4];
         DiceRoll diceRoll = new DiceRoll();
         String tileGeoType = "Blank";
         int roll = 0;
-        int maxRoll = 1;
+        int maxRoll = 100;
         
         switch (num) {
-            case 1, 11, 21, 31, 41, 51, 61 -> {System.out.println("Barren Plains"); tileGeoType = "Gray"; roll = diceRoll.roll(maxRoll);}
-            case 2, 12, 22, 32, 42, 52, 62, 72 -> {System.out.println("Scrubland Plains"); tileGeoType = "Yellow"; roll = diceRoll.roll(maxRoll);}
-            case 3, 23, 43, 63 -> {System.out.println("Forested Plains"); tileGeoType = "Green"; roll = diceRoll.roll(maxRoll);}
-            case 4, 14, 24, 34, 44, 54, 64, 74 -> {System.out.println("Barren Hills"); tileGeoType = "Gray"; roll = diceRoll.roll(maxRoll);}
-            case 5, 15, 25, 35, 45, 55, 65, 75 -> {System.out.println("Scrubland Hills"); tileGeoType = "Yellow"; roll = diceRoll.roll(maxRoll);}
-            case 6, 26, 46, 66 -> {System.out.println("Grassy Hills"); tileGeoType = "Green"; roll = diceRoll.roll(maxRoll);}
-            case 7, 37, 67 -> {System.out.println("Barren Mountains"); tileGeoType = "Gray"; roll = diceRoll.roll(maxRoll);}
-            case 8, 38 -> {System.out.println("Barren Swamps"); tileGeoType = "Gray"; roll = diceRoll.roll(maxRoll);}
-            case 9, 39, 69 -> {System.out.println("Barren Badlands"); tileGeoType = "Gray"; roll = diceRoll.roll(maxRoll);}
-            case 13, 33, 53, 73 -> {System.out.println("Grassy Plains"); tileGeoType = "Green"; roll = diceRoll.roll(maxRoll);}
-            case 16, 36, 56, 76 -> {System.out.println("Forested Hills"); tileGeoType = "Green"; roll = diceRoll.roll(maxRoll);}
-            case 17, 47 -> {System.out.println("Scrubland Mountains"); tileGeoType = "Yellow"; roll = diceRoll.roll(maxRoll);}
-            case 18, 48 -> {System.out.println("Scrubland Swamps"); tileGeoType = "Yellow"; roll = diceRoll.roll(maxRoll);}
-            case 19, 49 -> {System.out.println("Scrubland Badlands"); tileGeoType = "Yellow"; roll = diceRoll.roll(maxRoll);}
-            case 27, 57 -> {System.out.println("Forested Mountains"); tileGeoType = "Green"; roll = diceRoll.roll(maxRoll);}
-            case 28, 58 -> {System.out.println("Grassy Swamps"); tileGeoType = "Green"; roll = diceRoll.roll(maxRoll);}
-            case 29, 59 -> {System.out.println("Grassy Bandlands"); tileGeoType = "Green"; roll = diceRoll.roll(maxRoll);}
-            case 68 -> {System.out.println("Swamp"); tileGeoType = "Gray"; roll = diceRoll.roll(maxRoll);}
-            case 71 -> {System.out.println("Desert Plains"); tileGeoType = "Yellow"; roll = diceRoll.roll(maxRoll);}
-            case 77 -> {System.out.println("Grassy Mountains"); tileGeoType = "Green"; roll = diceRoll.roll(maxRoll);}
-            case 78 -> {System.out.println("Forested Swamps"); tileGeoType = "Green"; roll = diceRoll.roll(maxRoll);}
-            case 79 -> {System.out.println("Forested Badlands"); tileGeoType = "Green"; roll = diceRoll.roll(maxRoll);}
+            case 1, 11, 21, 31, 41, 51, 61 -> {tileGeoType = "Barren_Plains"; roll = diceRoll.roll(maxRoll);}
+            case 2, 12, 22, 32, 42, 52, 62, 72 -> {tileGeoType = "Scrubland_Plains"; roll = diceRoll.roll(maxRoll);}
+            case 3, 23, 43, 63 -> {tileGeoType = "Forested_Plains"; roll = diceRoll.roll(maxRoll);}
+            case 4, 14, 24, 34, 44, 54, 64, 74 -> {tileGeoType = "Barren_Hills"; roll = diceRoll.roll(maxRoll);}
+            case 5, 15, 25, 35, 45, 55, 65, 75 -> {tileGeoType = "Scrubland_Hills"; roll = diceRoll.roll(maxRoll);}
+            case 6, 26, 46, 66 -> {tileGeoType = "Grassy_Hills"; roll = diceRoll.roll(maxRoll);}
+            case 7, 37, 67 -> {tileGeoType = "Barren_Mountains"; roll = diceRoll.roll(maxRoll);}
+            case 8, 38 -> {tileGeoType = "Barren_Swamps"; roll = diceRoll.roll(maxRoll);}
+            case 9, 39, 69 -> {tileGeoType = "Barren_Badlands"; roll = diceRoll.roll(maxRoll);}
+            case 13, 33, 53, 73 -> {tileGeoType = "Grassy_Plains"; roll = diceRoll.roll(maxRoll);}
+            case 16, 36, 56, 76 -> {tileGeoType = "Forested_Hills"; roll = diceRoll.roll(maxRoll);}
+            case 17, 47 -> {tileGeoType = "Scrubland_Mountains"; roll = diceRoll.roll(maxRoll);}
+            case 18, 48 -> {tileGeoType = "Scrubland_Swamps"; roll = diceRoll.roll(maxRoll);}
+            case 19, 49 -> {tileGeoType = "Scrubland_Badlands"; roll = diceRoll.roll(maxRoll);}
+            case 27, 57 -> {tileGeoType = "Forested_Mountains"; roll = diceRoll.roll(maxRoll);}
+            case 28, 58 -> {tileGeoType = "Grassy_Swamps"; roll = diceRoll.roll(maxRoll);}
+            case 29, 59 -> {tileGeoType = "Grassy_Badlands"; roll = diceRoll.roll(maxRoll);}
+            case 68 -> {tileGeoType = "Swamps"; roll = diceRoll.roll(maxRoll);}
+            case 71 -> {tileGeoType = "Desert_Plains"; roll = diceRoll.roll(maxRoll);}
+            case 77 -> {tileGeoType = "Grassy_Mountains"; roll = diceRoll.roll(maxRoll);}
+            case 78 -> {tileGeoType = "Forested_Swamps"; roll = diceRoll.roll(maxRoll);}
+            case 79 -> {tileGeoType = "Forested_Badlands"; roll = diceRoll.roll(maxRoll);}
             //large cases
-            case 81 -> {System.out.println("Forested Plains, 1d10×20 squares"); tileGeoType = "Green"; roll = diceRoll.roll(maxRoll*2);}
-            case 82 -> {System.out.println("Scrubland Plains, 1d10×20 squares"); tileGeoType = "Yellow"; roll = diceRoll.roll(maxRoll*2);}
-            case 83 -> {System.out.println("Barren Plains, 1d10×20 squares"); tileGeoType = "Gray"; roll = diceRoll.roll(maxRoll*2);}
-            case 84 -> {System.out.println("Grassy Hills, 1d10×20 squares"); tileGeoType = "Green"; roll = diceRoll.roll(maxRoll*2);}
-            case 85 -> {System.out.println("Scrubland Hills, 1d10×20 squares"); tileGeoType = "Yellow"; roll = diceRoll.roll(maxRoll*2);}
-            case 86 -> {System.out.println("Barren Hills, 1d10×20 squares"); tileGeoType = "Gray"; roll = diceRoll.roll(maxRoll*2);}
-            case 87 -> {System.out.println("Scrubland Mountains, 1d10×20 squares"); tileGeoType = "Yellow"; roll = diceRoll.roll(maxRoll*2);}
-            case 88 -> {System.out.println("Scrubland Swamps, 1d10×20 squares"); tileGeoType = "Yellow"; roll = diceRoll.roll(maxRoll*2);}
-            case 89 -> {System.out.println("Scrubland Badlands, 1d10×20 squares"); tileGeoType = "Yellow"; roll = diceRoll.roll(maxRoll*2);}
-            case 91 -> {System.out.println("Grassy Plains, 1d10×50 squares"); tileGeoType = "Green"; roll = diceRoll.roll(maxRoll*5);}
-            case 92 -> {System.out.println("Scrubland Plains, 1d10×50 squares"); tileGeoType = "Yellow"; roll = diceRoll.roll(maxRoll*5);}
-            case 93 -> {System.out.println("Barren Plains, 1d10×50 squares"); tileGeoType = "Gray"; roll = diceRoll.roll(maxRoll*5);}
-            case 94 -> {System.out.println("Forested Hills, 1d10×50 squares"); tileGeoType = "Green"; roll = diceRoll.roll(maxRoll*5);}
-            case 95 -> {System.out.println("Scrubland Hills, 1d10×50 squares"); tileGeoType = "Yellow"; roll = diceRoll.roll(maxRoll*5);}
-            case 96 -> {System.out.println("Barren Hills, 1d10×50 squares"); tileGeoType = "Gray"; roll = diceRoll.roll(maxRoll*5);}
-            case 97 -> {System.out.println("Barren Mountains, 1d10×50 squares"); tileGeoType = "Gray"; roll = diceRoll.roll(maxRoll*5);}
-            case 98 -> {System.out.println("Scrubland Swamps, 1d10×50 squares"); tileGeoType = "Yellow"; roll = diceRoll.roll(maxRoll*5);}
-            case 99 -> {System.out.println("Barren Badlands, 1d10×50 squares"); tileGeoType = "Gray"; roll = diceRoll.roll(maxRoll*5);}
-            case 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 -> {System.out.println("River"); tileGeoType = "Blank";}
-            default -> {System.out.println("Special Feture"); tableSpecialFeatures(); tileGeoType = "Blank";} // Default case
+            case 81 -> {tileGeoType = "Forested_Plains"; roll = diceRoll.roll(maxRoll*2);}
+            case 82 -> {tileGeoType = "Scrubland_Plains"; roll = diceRoll.roll(maxRoll*2);}
+            case 83 -> {tileGeoType = "Barren_Plains"; roll = diceRoll.roll(maxRoll*2);}
+            case 84 -> {tileGeoType = "Grassy_Hills"; roll = diceRoll.roll(maxRoll*2);}
+            case 85 -> {tileGeoType = "Scrubland_Hills"; roll = diceRoll.roll(maxRoll*2);}
+            case 86 -> {tileGeoType = "Barren_Hills"; roll = diceRoll.roll(maxRoll*2);}
+            case 87 -> {tileGeoType = "Scrubland_Mountains"; roll = diceRoll.roll(maxRoll*2);}
+            case 88 -> {tileGeoType = "Scrubland_Swamps"; roll = diceRoll.roll(maxRoll*2);}
+            case 89 -> {tileGeoType = "Scrubland_Badlands"; roll = diceRoll.roll(maxRoll*2);}
+            case 91 -> {tileGeoType = "Grassy_Plains"; roll = diceRoll.roll(maxRoll*5);}
+            case 92 -> {tileGeoType = "Scrubland_Plains"; roll = diceRoll.roll(maxRoll*5);}
+            case 93 -> {tileGeoType = "Barren_Plains"; roll = diceRoll.roll(maxRoll*5);}
+            case 94 -> {tileGeoType = "Forested_Hills"; roll = diceRoll.roll(maxRoll*5);}
+            case 95 -> {tileGeoType = "Scrubland_Hills"; roll = diceRoll.roll(maxRoll*5);}
+            case 96 -> {tileGeoType = "Barren_Hills"; roll = diceRoll.roll(maxRoll*5);}
+            case 97 -> {tileGeoType = "Barren_Mountains"; roll = diceRoll.roll(maxRoll*5);}
+            case 98 -> {tileGeoType = "Scrubland_Swamps"; roll = diceRoll.roll(maxRoll*5);}
+            case 99 -> {tileGeoType = "Barren_Badlands"; roll = diceRoll.roll(maxRoll*5);}
+            case 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 -> {tileGeoType = "River"; roll = diceRoll.roll(maxRoll/10);}
+            default -> {tileGeoType = tableSpecialFeatures(); roll = 1;} // Default case
         }
         
-        typeAndNum[0] = tileGeoType;
-        typeAndNum[1] = roll;
+        //System.out.println("Number of tiles: " + roll);
         
-        return typeAndNum;
+        tileInfo[0] = runningBonus;
+        tileInfo[1] = tileGeoType;
+        tileInfo[2] = roll;
+        
+        return tileInfo;
     }
     
-    public void tableSpecialFeatures(){
+    public String tableSpecialFeatures(){
         DiceRoll diceRoll = new DiceRoll();
         int roll = diceRoll.roll(10);
+        String SpecialFeature = "";
         
         switch(roll){
-            case 1 -> System.out.println("Caves, d% squares");
+            case 1 -> System.out.println("Caves");
             case 2 -> System.out.println("Cliff");
             case 3 -> System.out.println("Fertile Valley");
-            case 4 -> System.out.println("Geyser");
-            case 5 -> System.out.println("Isolated Mountain");
-            case 6 -> System.out.println("Pool");
-            case 7 -> System.out.println("Tor");
-            case 8 -> System.out.println("Volcano");
-            case 9 -> System.out.println("Waterfall");
-            case 10 -> System.out.println("Whirlpool");
+            case 4 -> System.out.println("Pool");
+            case 5 -> System.out.println("Tor");
+            case 6 -> System.out.println("Volcano");
+            case 7 -> System.out.println("Waterfall");
+            case 8 -> System.out.println("Whirlpool");
         }
+        return SpecialFeature;
     }
 }
