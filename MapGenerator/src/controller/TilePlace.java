cbd94c2c184ab0592df436897926e84f1b68d22f
SugMadicx <147711385+SugMadicx@users.noreply.github.com>
2023-11-20 15:52:58 +0100

    updated placement logic
    debugged method. Still doesn't fill full grid.
    
    Exception in thread "AWT-EventQueue-0" java.lang.NullPointerException: Cannot load from object array because "this.tileInformation" is null
    	at model.Cell.SetTileImages(Cell.java:43)

--- MapGenerator/src/controller/TilePlace.java
+++ MapGenerator/src/controller/TilePlace.java
@@ -14,30 +14,40 @@ public class TilePlace {
     private Set<Integer> blocked = new HashSet<>();
   
     public Object[][] placeTile(int x,int y) {
+        System.out.println("initialize place tile method.");
         int runningBonus = 0;
         int maxTiles = x * y;
         Object[][] grid = new Object[x][y];
-        Object[] tilePack = new Object[2];
-        int xPos = diceRoll.roll(x), yPos = diceRoll.roll(y);
+        Object[] tilePack = new Object[4];
+        int xPos = diceRoll.roll(x - 1), yPos = diceRoll.roll(y - 1);
         
         while (maxTiles > 0){
+            System.out.println("Das ist ein RuningBonus" + runningBonus);
             tilePack = geography.geographyGen(runningBonus);
             if( (int) tilePack[2] > maxTiles){
                 tilePack[2] = maxTiles;
                 xPos = 0;
                 yPos = 0;
-                while (xPos <= x){
-                    while(yPos <= y){
+                while (xPos < x){
+                    System.out.println("xPos:" + xPos + "yPos:" + yPos);
+                    while(yPos < y){
                         if (grid[xPos][yPos] == null){grid[xPos][yPos] = (String) tilePack[1];}
                         yPos++;
                     }
                     xPos++;
+                }
+                if(xPos == x){
+                    xPos--;
+                }
+                if(yPos == y){
+                    yPos--;
                 }
             }
             runningBonus = (int) tilePack[0];
             int downCounter = (int) tilePack[2];
             
             // If this is the first tile or a random condition is met, choose a random position
+            System.out.println("Grid printout:" + grid[xPos][yPos]);
             if (grid[xPos][yPos] != null) {
                 xPos = diceRoll.roll(grid.length);
                 yPos = diceRoll.roll(grid[0].length);
@@ -70,5 +80,5 @@ public class TilePlace {
         }
 
         return grid;
-    }  
+    }   
 }
