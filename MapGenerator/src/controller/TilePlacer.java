f35280b94234dbe243d8ce05e622aa5dc9da6927
NeuralDecker <jason.a.charles@outlook.com>
2023-11-23 13:26:29 +0100

    For Flo

--- MapGenerator/src/controller/TilePlacer.java
+++ MapGenerator/src/controller/TilePlacer.java
@@ -12,13 +12,21 @@ public class TilePlacer {
     private Geography geography = new Geography();
     private DiceRoll diceRoll = new DiceRoll();
 
+    //for testing in console
+    public static void main(String[] args) {
+        TilePlacer tilePlacer = new TilePlacer();
+        Object[][] grid = new Object[20][20];
+        int x = 20;
+        int y = 20;
+        grid = tilePlacer.placeTileSet(x, y);
+    }
+
     public Object[][] placeTileSet(int x, int y) {
         System.out.println("Initialize placeTileSet method...");
         int runningBonus = 0;
         int maxTiles = x * y;
         Object[][] grid = new Object[x][y];
         Object[] tileSet = new Object[3];
-        Object[] newCoordinates = new Object[2];
 
         //set random position
         int xPos = diceRoll.roll(x - 1);
@@ -26,16 +34,14 @@ public class TilePlacer {
 
         //while there is still space in the grid
         while (maxTiles > 0) {
-            System.out.println("RuningBonus: " + runningBonus);
-
             //get new TileSet
             tileSet = geography.geographyGen(runningBonus);
+            int[] newCoordinates = new int[(int) tileSet[2] * 2];
 
             //at almost end check what is left, compare tileSet[2] "amount" with rest of maxTiles
             //----------------------------------------------- dose not enter ??? ----------------------------------------------------------------
             if ((int) tileSet[2] > maxTiles) {
                 tileSet[2] = maxTiles; //set amount to what is left
-
                 //set coordinates 0,0
                 xPos = 0;
                 yPos = 0;
@@ -48,118 +54,271 @@ public class TilePlacer {
                         if (grid[xPos][yPos] == null) {
                             grid[xPos][yPos] = (String) tileSet[1];
                         }
-                        yPos++;
+                        if (yPos < y) {
+                            yPos++;
+                        }
                     }
-                    xPos++;
+                    if (xPos < x) {
+                        xPos++;
+                    }
                 }
 
-                //reset to in bound for array
-                if (xPos == x) {
-                    xPos--;
-                }
-                if (yPos == y) {
-                    yPos--;
-                }
             }
 
             runningBonus = (int) tileSet[0]; //get new running Boni
 
-            int downCounter = (int) tileSet[2]; //set for cound down
-
-            System.out.println("xPos:" + xPos + "yPos:" + yPos);
+            int tilesToPlaceNum = (int) tileSet[2];
+            int upCounter = 0;
 
             //while not empty, choose a random position
             while (grid[xPos][yPos] != null) {
-                xPos = diceRoll.roll(grid.length);
-                yPos = diceRoll.roll(grid[0].length);
-                System.out.println("new xPos:" + xPos + "new yPos:" + yPos);
+                System.out.println("no empty cell, reroll.");
+                xPos = diceRoll.roll(x - 1);
+                yPos = diceRoll.roll(y - 1);
             }
+
+            System.out.println("current Co:" + xPos + ", " + yPos);
+
+            //get spiral coordinates
+            newCoordinates = goAround(x, y, xPos, yPos, (tilesToPlaceNum - upCounter));
 
             // Otherwise, choose a position around the last placed tile 
             grid[xPos][yPos] = (String) tileSet[1]; //insert tile type into cell
-            downCounter--;
+            upCounter++;
 
-            while (downCounter > 0) {
-                int newXPos = 0;
-                int newYPos = 0;
-                int direction = 1;
+            int newXPos = 0;
+            int newYPos = 0;
+            int nextCoPosition = 0;
+            System.out.println("tilesToPlaceNum: " + tilesToPlaceNum);
+
+            while (upCounter < tilesToPlaceNum) {
+                System.out.println("uPCounter: " + upCounter);
+                System.out.println("tilesToPlaceNum: " + tilesToPlaceNum);
+                System.out.println("readout next coordinates");
+                //save new positions
+                System.out.println("nextCoPosition: " + nextCoPosition);
+                newXPos = (int) newCoordinates[nextCoPosition];
+                newYPos = (int) newCoordinates[nextCoPosition + 1];
+                
+                //System.out.println("newXPos: " + newXPos);
+                //System.out.println("newYPos: " + newYPos);
 
                 //check if is in grid bound
-                if (newXPos < 0) {
-                    newXPos++;
-                }
-                if (newYPos < 0) {
-                    newYPos++;
-                }
-                if (newXPos >= x) {
-                    newXPos--;
-                }
-                if (newYPos >= y) {
-                    newYPos--;
-                }
-                
-                //TODO - place around untill full spiral or hits not null
-                //while cell null and not out of bounds
-                while (grid[xPos][yPos] == null) {
-
-                    
-                    
+                if (newXPos > 0 && newYPos > 0 && newXPos < x && newYPos < y) {
+                    //adapt to new position
                     xPos = newXPos;
                     yPos = newYPos;
-                    grid[xPos][yPos] = (String) tileSet[1]; //insert tile type into cell
-                    downCounter--;
 
-                    newCoordinates = goAround(xPos, yPos, direction);
-                    direction++;
-
-                    //save new positions
-                    newXPos = (int) newCoordinates[0];
-                    newYPos = (int) newCoordinates[1];
-
-                    //TODO move to new outer circle
+                    //if cell null
+                    if (grid[xPos][yPos] == null) {
+                        grid[xPos][yPos] = (String) tileSet[1]; //insert tile type into cell
+                        upCounter++;
+                    }
                 }
+                
+                //go to next coordinates position in newCoordinets
+                nextCoPosition += 2;
             }
             //reduce maxTile of amount in tileSet[2]
             maxTiles = maxTiles - (int) tileSet[2];
         }
-
         //return filled grid
         return grid;
     }
 
-    public Object[] goAround(int oldX, int oldY, int direction) {
-        Object[] coordinates = new Object[2];
+    public int[] goAround(int gridXMax, int gridYMax, int startX, int startY, int tileNum) {
+        int[] result = new int[tileNum * 2];
+        int x = startX;
+        int y = startY;
+        int step = 0;
+        int[] co = new int[2];
 
-        switch (direction) {
-            case 1:
-                coordinates[0] = oldX - 1;
-                break; // left
-            case 2:
-                coordinates[0] = oldX - 1;
-                coordinates[1] = oldY + 1;
-                break; // left-up
-            case 3:
-                coordinates[1] = oldY + 1;
-                break; // up
-            case 4:
-                coordinates[0] = oldX + 1;
-                coordinates[1] = oldY + 1;
-                break; // up-right
-            case 5:
-                coordinates[0] = oldX + 1;
-                break; // right
-            case 6:
-                coordinates[0] = oldX + 1;
-                coordinates[1] = oldY - 1;
-                break; // Down-right
-            case 7:
-                coordinates[1] = oldY - 1;
-                break; // down
-            default:
-                coordinates[0] = oldX - 1;
-                coordinates[1] = oldY - 1;
-                break; // down-left
+        System.out.println("Start step: " + step);
+        
+        // Store initial position
+        result[0] = x;
+        //System.out.println("First X result: " + result[0]);
+        result[++step] = y;
+        //System.out.println("First Y result: " + result[1]);
+        
+        //System.out.println("First step: " + step);
+        
+
+        System.out.println("how many tiles?: " + tileNum);
+
+        for (int i = 2; i < result.length; i = i + step) {
+            System.out.println("next step");
+            for (int layer = 0; layer < Math.min(gridXMax, gridYMax); layer += 2) {
+                System.out.println("generate spiral");
+                co = moveLeft(x, y);
+                if (step < result.length - 1) {
+                    //System.out.println("first x in : " + x);
+                    result[++step] = co[0];
+                    x = co[0];
+                    //System.out.println("x: " + x);
+                    //System.out.println("result: " + result[step]);
+                    if(step == result.length - 1){return result;} 
+                }
+                if (step < result.length - 1) {
+                    result[++step] = co[1];
+                    y = co[1];
+                    //System.out.println("result: " + result[step]);
+                    if(step == result.length - 1){return result;} 
+                }
+
+                for (int ii = layer; ii >= 0; ii -= 2) {
+                    //System.out.println("move up");
+                    co = moveUp(x, y);
+                    if (step < result.length - 1) {
+                        result[++step] = co[0];
+                        x = co[0];
+                        //System.out.println("result: " + result[step]);
+                        if(step == result.length - 1){return result;} 
+                    }
+                    if (step < result.length - 1) {
+                        result[++step] = co[1];
+                        y = co[1];
+                        //System.out.println("result: " + result[step]);
+                        if(step == result.length - 1){return result;} 
+                    }
+                    if (ii >= 2) {
+                        //System.out.println("move up");
+                        co = moveUp(x, y);
+                        if (step < result.length - 1) {
+                            result[++step] = co[0];
+                            x = co[0];
+                            //System.out.println("result: " + result[step]);
+                            if(step == result.length - 1){return result;} 
+                        }
+                        if (step < result.length - 1) {
+                            result[++step] = co[1];
+                            y = co[1];
+                            //System.out.println("result: " + result[step]);
+                            if(step == result.length - 1){return result;} 
+                        }
+                    }
+                }
+                for (int ii = layer; ii >= 0; ii -= 2) {
+                    //System.out.println("move right");
+                    co = moveRight(x, y);
+                    if (step < result.length - 1) {
+                        result[++step] = co[0];
+                        x = co[0];
+                        //System.out.println("result: " + result[step]);
+                        if(step == result.length - 1){return result;} 
+                    }
+                    if (step < result.length - 1) {
+                        result[++step] = co[1];
+                        y = co[1];
+                        //System.out.println("result: " + result[step]);
+                        if(step == result.length - 1){return result;} 
+                    }
+                    //System.out.println("move right");
+                    co = moveRight(x, y);
+                    if (step < result.length - 1) {
+                        result[++step] = co[0];
+                        x = co[0];
+                        //System.out.println("result: " + result[step]);
+                        if(step == result.length - 1){return result;} 
+                    }
+                    if (step < result.length - 1) {
+                        result[++step] = co[1];
+                        y = co[1];
+                        //System.out.println("result: " + result[step]);
+                        if(step == result.length - 1){return result;} 
+                    }
+                }
+                for (int ii = layer; ii >= 0; ii -= 2) {
+                    //System.out.println("move down");
+                    co = moveDown(x, y);
+                    if (step < result.length - 1) {
+                        result[++step] = co[0];
+                        x = co[0];
+                        //System.out.println("result: " + result[step]);
+                        if(step == result.length - 1){return result;} 
+                    }
+                    if (step < result.length - 1) {
+                        result[++step] = co[1];
+                        y = co[1];
+                        //System.out.println("result: " + result[step]);
+                        if(step == result.length - 1){return result;} 
+                    }
+                    //System.out.println("move down");
+                    co = moveDown(x, y);
+                    if (step < result.length - 1) {
+                        result[++step] = co[0];
+                        x = co[0];
+                        //System.out.println("result: " + result[step]);
+                        if(step == result.length - 1){return result;} 
+                    }
+                    if (step < result.length - 1) {
+                        result[++step] = co[1];
+                        y = co[1];
+                        //System.out.println("result: " + result[step]);
+                        if(step == result.length - 1){return result;} 
+                    }
+                }
+                for (int ii = layer; ii >= 0; ii -= 2) {
+                    //System.out.println("move left");
+                    co = moveLeft(x, y);
+                    if (step < result.length - 1) {
+                        result[++step] = co[0];
+                        x = co[0];
+                        //System.out.println("result: " + result[step]);
+                        if(step == result.length - 1){return result;} 
+                    }
+                    if (step < result.length - 1) {
+                        result[++step] = co[1];
+                        y = co[1];
+                        //System.out.println("result: " + result[step]);
+                        if(step == result.length - 1){return result;} 
+                    }
+                    //System.out.println("move left");
+                    co = moveLeft(x, y);
+                    if (step < result.length - 1) {
+                        result[++step] = co[0];
+                        x = co[0];
+                        //System.out.println("result: " + result[step]);
+                        if(step == result.length - 1){return result;} 
+                    }
+                    if (step < result.length - 1) {
+                        result[++step] = co[1];
+                        y = co[1];
+                        //System.out.println("result: " + result[step]);
+                        if(step == result.length - 1){return result;} 
+                    }
+                }
+            }
         }
-        return coordinates;
+
+        return result;
+    }
+
+    public int[] moveLeft(int x, int y) {
+        int[] co = new int[2];
+        co[0] = --x;
+        co[1] = y;
+        return co;
+    }
+
+    public int[] moveRight(int x, int y) {
+        int[] co = new int[2];
+        co[0] = ++x;
+        co[1] = y;
+        return co;
+    }
+
+    public int[] moveUp(int x, int y) {
+        int[] co = new int[2];
+        co[0] = x;
+        co[1] = --y;
+        return co;
+    }
+
+    public int[] moveDown(int x, int y) {
+        int[] co = new int[2];
+        co[0] = x;
+        co[1] = ++y;
+        return co;
     }
 }
